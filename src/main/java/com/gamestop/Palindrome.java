add66c76d67192486a3c90c094140b37abe03ec9
Tp0909 <test.practice0909@gmail.com>
2023-11-25 14:49:52 -0500

    second

--- src/main/java/com/gamestop/Palindrome.java
+++ src/main/java/com/gamestop/Palindrome.java
@@ -1,4 +1,4 @@
-public class PalindromeChecker {
+public class Palindrome{
     public static void main(String[] args) {
         String str = "madam"; // Change this string to test different inputs
 
